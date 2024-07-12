bc9e75e3434d4093f9c0a1737d4dbf378fada253
Abdykali Azat <azat.abdykalli@gmail.com>
2024-07-12 19:03:21 +0500

    first commit

---
+++ src/config.js
@@ -0,0 +1,11 @@
+// config.js
+const config = {
+    // apiUrl: 'http://localhost:3001',
+    apiUrl: 'https://ailanacargo.kz:3001',
+    
+    nameCargo: 'Ailana-Cargo-Astana'
+  };
+  
+  
+export default config;
+  
